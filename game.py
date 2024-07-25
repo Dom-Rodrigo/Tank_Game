f6fa85b349387196c02d9cb86927d2e4c637485b
Dom-Rodrigo <131702125+Dom-Rodrigo@users.noreply.github.com>
2024-07-25 15:46:57 -0300

    Cleaning code

--- game.py
+++ game.py
@@ -4,16 +4,16 @@ from bullet import Bullet
 
 pygame.mixer.init()
 pygame.init()
-#sound of bullets
+
+# sound of bullets
 sound_bullet=pygame.mixer.Sound("Pistola22cal.wav")
-# Carrega o som de destruição
+
+# destruction sounds
 destroy = pygame.mixer.Sound("explosao.wav")
 
 size = width, height = 900, 700
-speed = [1, 0]
 tank_width, tank_height = 100, 100
 grey = 112, 112, 112
-i = 0
 
 screen = pygame.display.set_mode(size)
 screen_rect = screen.get_rect()
@@ -67,22 +67,7 @@ tanks.add(tank)
 tanks.add(tank1)
 tanks.add(tank2)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-
-    screen.fill(grey)
-
-    tanks_points = []
-    for tank in tanks:
-        tanks_points.append(tank)
-
-    font = pygame.font.Font(None, 40)
-    placar = font.render(f"Green {tanks_points[0].points} x Red {tanks_points[1].points} x Blue {tanks_points[2].points}", True, (20, 20, 20))
-    screen.blit(placar, (width//2 - placar.get_width()//2, 0))
-
-
+def movement(tanks, screen, keys):
     for tank in tanks:
         screen.blit(tank.image, tank.rect)
         tank.check_ifout(screen_rect)
@@ -91,8 +76,6 @@ while True:
         if tank.space_pressed == 1:
             tank.bullets.update()
 
-
-        keys = pygame.key.get_pressed()
         start_time = pygame.time.get_ticks()
 
         if tank.turn == 4 or tank.turn == -4:  # one cycle
@@ -108,47 +91,68 @@ while True:
             if keys[tank.k_right]:
                 tank.move(turn_right=True)
 
+def firing(tanks, screen, keys):
+        for tank in tanks:
+            screen.blit(tank.image, tank.rect)
+            current_time = pygame.time.get_ticks()
+
+            if current_time > tank.next_bullet_time:
+                if keys[tank.k_fire]:
+                    if len(tank.bullets) < 10:
+                        tank.bullets.add(Bullet(bimg, tank))
+                    else:
+                        pass
+                    print(len(tank.bullets))
+                    tank.space_pressed = 1
+                    sound_bullet.play()
+                tank.next_bullet_time += tank.timer_interval
+
+                # WORKS UNTIL HERE
+                for bullet in tank.bullets:
+                    if pygame.sprite.spritecollideany(bullet, tanks):
+
+                        collided = pygame.sprite.spritecollideany(bullet, tanks)
+                        collided.endurance -= 1
+                        if collided.endurance == 0:
+                            bullet.tank.bullets.empty()
+                            #collided.speed = 0
+                            #collided.rect.x = collided.rect.x - 16
+                            #collided.rect.y = collided.rect.y - 16
+                            screen.blit(collided.destroyed_image, (collided.rect.x-16, collided.rect.y-16))
+                            bullet.tank.points +=1
+                            collided.endurance = 60
+                            i = 0;
+                            for tank in tanks:
+                                tank.update(positions[i][0], positions[i][1])
+                                i+=1
+                            if show_game_over_message():
+                                print("on restart")
+                                # RENEW THE TANKS
+                                ##collided = Tank(tank1_image, tank1_destroyed_image, speed, x=width, y=height, points, endurance,  k_up, k_down, k_left, k_right, k_fire, space_pressed, next_bullet_time, timer_interval)
+                                #pygame.time.delay(500)
+                            else:
+                                pygame.quit()
+                                sys.exit()
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    screen.fill(grey)
+
+    tanks_points = []
     for tank in tanks:
-        screen.blit(tank.image, tank.rect)
-        current_time = pygame.time.get_ticks()
+        tanks_points.append(tank)
 
-        if current_time > tank.next_bullet_time:
-            if keys[tank.k_fire]:
-                if len(tank.bullets) < 10:
-                    tank.bullets.add(Bullet(bimg, tank))
-                else:
-                    pass
-                print(len(tank.bullets))
-                tank.space_pressed = 1
-                sound_bullet.play()
-            tank.next_bullet_time += tank.timer_interval
+    font = pygame.font.Font(None, 40)
+    placar = font.render(f"Green {tanks_points[0].points} x Red {tanks_points[1].points} x Blue {tanks_points[2].points}", True, (20, 20, 20))
+    screen.blit(placar, (width//2 - placar.get_width()//2, 0))
 
-            # WORKS UNTIL HERE
-            for bullet in tank.bullets:
-                if pygame.sprite.spritecollideany(bullet, tanks):
+    keys = pygame.key.get_pressed()
+    movement(tanks, screen, keys)
+    firing(tanks, screen, keys)
 
-                    collided = pygame.sprite.spritecollideany(bullet, tanks)
-                    collided.endurance -= 1
-                    if collided.endurance == 0:
-                        bullet.tank.bullets.empty()
-                        #collided.speed = 0
-                        #collided.rect.x = collided.rect.x - 16
-                        #collided.rect.y = collided.rect.y - 16
-                        screen.blit(collided.destroyed_image, (collided.rect.x-16, collided.rect.y-16))
-                        bullet.tank.points +=1
-                        collided.endurance = 60
-                        i = 0;
-                        for tank in tanks:
-                            tank.update(positions[i][0], positions[i][1])
-                            i+=1
-                        if show_game_over_message():
-                            print("on restart")
-                            # RENEW THE TANKS
-                            ##collided = Tank(tank1_image, tank1_destroyed_image, speed, x=width, y=height, points, endurance,  k_up, k_down, k_left, k_right, k_fire, space_pressed, next_bullet_time, timer_interval)
-                            #pygame.time.delay(500)
-                        else:
-                            pygame.quit()
-                            sys.exit()
+
 
     clock.tick(100)
     pygame.display.flip()
